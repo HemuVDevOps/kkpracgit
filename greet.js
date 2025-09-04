b49f6a445721b4ae48c126e80b479e3fc37a3ced
Hemanth Varupula <varupulahemanth@gmailo.com>
2025-09-05 05:24:30 +0530

    some cpp code is added

--- greet.js
+++
@@ -1,9 +0,0 @@
-console.log("hello world")
-function add (){}[D
-[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
-[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
-  consle.log("hello from function ")
-}
-add();
-[A[C[C[C[C[C[;
-
