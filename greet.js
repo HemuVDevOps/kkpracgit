0f06cb33d2e900f6c11e05f547691133ad7ac04a
Hemanth Varupula <varupulahemanth@gmailo.com>
2025-09-05 03:27:10 +0530

    greet.js is updated

--- greet.js
+++ greet.js
@@ -1,1 +1,9 @@
 console.log("hello world")
+function add (){}[D
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
+  consle.log("hello from function ")
+}
+add();
+[A[C[C[C[C[C[;
+
