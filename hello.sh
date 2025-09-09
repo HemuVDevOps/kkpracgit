3e0d38a94c2c33a84cd30512f39b27dd2309611e
Hemanth Varupula <varupulahemanth@gmailo.com>
2025-09-10 03:58:19 +0530

    hello.sh is added

---
+++ hello.sh
@@ -0,0 +1,41 @@
+# echo "hii"
+# age=99
+# if [ $age -gt 19 ];then
+# echo "adult"
+# else
+# echo "minor"
+# fi
+
+# add()
+# {
+# echo "hello"
+# }
+# add
+
+
+# for i in 1 2 3 4 5 6
+# do
+# echo "$i"
+# done
+
+# sub (){
+#     echo $(( $1 - $2 ))
+# }
+
+# sub $1 $2
+
+
+
+read -p "reading age " age
+echo "$age"
+
+
+my_array=($1 $2 $3)
+
+for j in "${my_array[@]}"
+do 
+echo "$j"
+done
+
+
+
