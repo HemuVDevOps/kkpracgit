8806dd5d14a5cf46157397f1392fef8d65492b4d
Hemanth Varupula <varupulahemanth@gmailo.com>
2025-09-25 05:47:04 +0530

    og added to hi

--- hi.js
+++ hi.js
@@ -1,8 +1,8 @@
 console.log("lol india will win asia cup 2025")
 console.log("mirai movie is a good sucess")
-<<<<<<< HEAD
-
 console.log("hello janaki"
-=======
 console.log("namste guru")
->>>>>>> main
+	function ojas(){
+		console.log("og from sujith")
+	}
+	ojas();
