45d5d1d250bc4c7e1743192a7dec4394e548c1c3
Hemanth Varupula <varupulahemanth@gmailo.com>
2025-09-25 05:44:51 +0530

    ramana func added

--- hi.js
+++ hi.js
@@ -1,8 +1,7 @@
 console.log("lol india will win asia cup 2025")
 console.log("mirai movie is a good sucess")
-<<<<<<< HEAD
-
 console.log("hello janaki"
-=======
 console.log("namste guru")
->>>>>>> main
+function Ramana(){
+	condole.log("ramana is from guntur karam ")
+}
